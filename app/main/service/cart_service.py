589afe6765273307eb8c3802c499c2559831edc0
sang cao <hansan.tech@gmail.com>
2022-03-30 23:39:11 +0700

    refactor code

--- app/main/service/cart_service.py
+++ app/main/service/cart_service.py
@@ -22,28 +22,34 @@ def save_new_cart(data):
                     itm.quantity += data.get("quantity")
                     itm.save()
             else:
-                # product=Product.query.filter_by(product_id=data.get("product_id")).first()
+                product=Product.query.filter_by(product_uuid=data.get("product_id")).first()
 
                 cart_item=CartItem()
                 cart_items.cart_id=cart_data.id
                 cart_items.cart_item_uuid=cart_data.cart_id
                 cart_item.product_uuid=data.get("product_id")
-                cart_item.subtotal_ex_tax=data.get("product_id")
-                cart_item.tax_total=int(data.get("quantity"))*10
+                cart_item.subtotal_ex_tax=int(data.get("quantity"))
+
+                if product:
+                    cart_item.tax_total=int(data.get("quantity"))*product.price
+                else:
+                    cart_item.tax_total=int(data.get("quantity"))
                 cart_item.total=data.get("quantity")
                 cart_item.quantity=int(data.get("quantity"))
                 save_changes(cart_item)
+
+                # return data as required
                 return jsonify(get_cart_by_user_id(user_id)), 200
 
     return {"message":"Bad request!!!"}, 403
 
-def change_cart_quantity():
+def change_cart_quantity(cart_item_id,json_data):
     pass
 
 def checkout_cart():
     pass
 
-def delete_cart_item():
+def delete_cart_item(cart_item_id):
     pass
 
 def save_changes(data):
