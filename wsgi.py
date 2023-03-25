cd29e58ecbca4670bcccf087efcf31f5fc1d0490
sang cao <caohoangsang.it@gmail.com>
2023-03-25 15:01:32 +0700

    update CORS allow http call to api

--- wsgi.py
+++ wsgi.py
@@ -4,10 +4,14 @@ from flask_script import Manager
 from app import blueprint
 from app.main import create_app, db
 from app.main.model import user, cart, cart_item
+from flask_cors import CORS
 
 app = create_app('dev')
-app.register_blueprint(blueprint)
 
+CORS(app, resources={r"*": {"origins": "*"}},  supports_credentials=True)
+# CORS(app, resources={r"/api/*": {"origins": "*"}},  supports_credentials=True)
+app.register_blueprint(blueprint)
+# ATBBW8W8ybUXdA4agxHZAGsH3UzB224D0D82
 app.app_context().push()
 
 manager = Manager(app)
